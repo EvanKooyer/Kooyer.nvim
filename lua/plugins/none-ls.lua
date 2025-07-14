7c39ea91594a291acf89a3551b7a9d103b6e7e37
Evan Kooyer <kooyee2@medtronic.com>
2025-07-14 12:02:46 -0500

    Fixed my mason-none-ls plugin to hopefully actually work

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -29,17 +29,39 @@ return {
       "nvimtools/none-ls.nvim",
     },
     config = function()
-      ensure_installed = {
-        "stylua",
-        "prettier",
-        "black",
-        "refactoring",
-        "texlint",
-        "Markdownlint",
-        "proselint",
-        "pylint",
-        "eslint_d",
+      local DEFAULT_SETTINGS = {
+        -- A list of sources to install if they're not already installed.
+        -- This setting has no relation with the `automatic_installation` setting.
+        ensure_installed = {
+          "stylua",
+          "prettier",
+          "black",
+          "refactoring",
+          "texlint",
+          "Markdownlint",
+          "proselint",
+          "pylint",
+          "eslint_d",
+        },
+        -- Enable or disable null-ls methods to get set up
+        -- This setting is useful if some functionality is handled by other plugins such as `conform` and `nvim-lint`
+        methods = {
+            diagnostics = true,
+            formatting = true,
+            code_actions = true,
+            completion = true,
+            hover = true,
+        },
+        -- Run `require("null-ls").setup`.
+        -- Will automatically install masons tools based on selected sources in `null-ls`.
+        -- Can also be an exclusion list.
+        -- Example: `automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }`
+        automatic_installation = false,
+        -- See [#handlers-usage](#handlers-usage) section
+        handlers = {},
       }
+
+      require("mason-null-ls").setup(DEFAULT_SETTINGS)
     end,
   }
 }
