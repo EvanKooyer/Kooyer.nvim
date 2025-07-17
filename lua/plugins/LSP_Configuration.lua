52a4cbdcbc1e52a0eeea1eb7107616034888f88b
Evan Kooyer <kooyee2@medtronic.com>
2025-07-17 10:48:26 -0500

    fixed some lsp things so my lsp works with markdown files

--- lua/plugins/LSP_Configuration.lua
+++ lua/plugins/LSP_Configuration.lua
@@ -26,18 +26,16 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       local lspconfig = require("lspconfig")
+
       lspconfig.lua_ls.setup({})
       lspconfig.zls.setup({})
       lspconfig.pyright.setup({})
       lspconfig.pico8_ls.setup({})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {desc='LSP Hover'})
-      vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {desc='Go to declaration'})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {desc='Go to definition'})
       vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {desc='List implementations'})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {desc='List References'})
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {desc='Rename all references'})
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {desc='Format by lint/LSP'})
+      vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {desc='Format by lint/LSP'})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {desc='Code Actions'})
     end,
   },
