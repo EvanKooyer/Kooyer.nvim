7bb9cc0d5a9086bb3afa3af9feb17f99762a3322
Evan Kooyer <kooyee2@medtronic.com>
2025-07-18 07:20:49 -0500

    made the render markdown plugin lazy load properly according to lazy.nvim specs

--- lua/plugins/markdown-tools.lua
+++ lua/plugins/markdown-tools.lua
@@ -5,7 +5,69 @@ return {
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
-    lazy = true,
-    event = 'BufEnter *.md'
+    ft = "markdown",
+    config = function()
+      vim.o.textwidth = 80
+
+      require("render-markdown").setup({
+        completions = {
+          blink = { enabled = true },
+        },
+
+        checkbox = {
+          enabled = true,
+          render_modes = false,
+          bullet = false,
+          right_pad = 1,
+          unchecked = {
+            icon = "󰄱 ",
+            highlight = "RenderMarkdownUnchecked",
+            scope_highlight = nil,
+          },
+          checked = {
+            icon = "󰱒 ",
+            highlight = "RenderMarkdownChecked",
+            scope_highlight = nil,
+          },
+          custom = {
+            todo = { raw = "[-]", rendered = "󰥔 ", highlight = "RenderMarkdownTodo", scope_highlight = nil },
+          },
+        },
+
+        link = {
+          enabled = true,
+          render_modes = false,
+          footnote = {
+            enabled = true,
+            superscript = true,
+            prefix = "",
+            suffix = "",
+          },
+          image = "󰥶 ",
+          email = "󰀓 ",
+          hyperlink = "󰌹 ",
+          highlight = "RenderMarkdownLink",
+          wiki = {
+            icon = "󱗖 ",
+            body = function()
+              return nil
+            end,
+            highlight = "RenderMarkdownWikiLink",
+          },
+          custom = {
+            web = { pattern = "^http", icon = "󰖟 " },
+            discord = { pattern = "discord%.com", icon = "󰙯 " },
+            github = { pattern = "github%.com", icon = "󰊤 " },
+            gitlab = { pattern = "gitlab%.com", icon = "󰮠 " },
+            google = { pattern = "google%.com", icon = "󰊭 " },
+            neovim = { pattern = "neovim%.io", icon = " " },
+            reddit = { pattern = "reddit%.com", icon = "󰑍 " },
+            stackoverflow = { pattern = "stackoverflow%.com", icon = "󰓌 " },
+            wikipedia = { pattern = "wikipedia%.org", icon = "󰖬 " },
+            youtube = { pattern = "youtube%.com", icon = "󰗃 " },
+          },
+        },
+      })
+    end
   },
 }
